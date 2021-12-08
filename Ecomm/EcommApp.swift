321d25ab5c62475cd1f65dbe8964c1aa7d9f7891
Antonio Orozco Sanchez <j.antonio.oro@gmail.comm>
2021-12-08 11:18:38 -0600

    Initial Commit

---
+++ Ecomm/EcommApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommApp.swift
+//  Ecomm
+//
+//  Created by Antonio Orozco on 08/12/21.
+//
+
+import SwiftUI
+
+@main
+struct EcommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
